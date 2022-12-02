33930719c6e65ac6ed62b96855b851c869d868f8
Hanbi58 <hanxiaonan2012@gmail.com>
2022-12-01 23:24:05 -0800

    WIP

--- src/sections/Shop.js
+++ src/sections/Shop.js
@@ -1,7 +1,56 @@
-import React from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import React, { useLayoutEffect } from "react";
+import styled from "styled-components";
+import { useRef } from "react";
+
+import { motion } from "framer-motion";
+
+import img1 from "../assets/Images/1.webp";
+import img2 from "../assets/Images/2.webp";
+import img3 from "../assets/Images/3.webp";
+import img4 from "../assets/Images/4.webp";
+import img5 from "../assets/Images/5.webp";
+import img6 from "../assets/Images/6.webp";
+import img7 from "../assets/Images/7.webp";
+import img8 from "../assets/Images/8.webp";
+import img9 from "../assets/Images/9.webp";
+import img10 from "../assets/Images/10.webp";
+
+const Section = styled.section`
+  min-height: 100vh;
+  height: auto;
+  width: 100vw;
+  margin: 0 auto;
+  overflow: hidden;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxxl};
+  font-family: "Kaushan Script";
+  font-weight: 300;
+  text-shadow: 1px 1px 1px ${(props) => props.theme.body};
+  color: ${(props) => props.theme.body};
+  position: absolute;
+  top: 1rem;
+  left: 5%;
+  z-index: 11;
+`;
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <Section>
+      <Title data-scroll data-scroll-speed="-1">
+        New Arrivals
+      </Title>
+    </Section>
+  );
 };
 
 export default Shop;
